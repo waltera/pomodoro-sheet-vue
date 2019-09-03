13992ee3b1882bb3e701a7022615146b29de6940
Walter Longo Neto <walterlongoneto@gmail.com>
2019-09-03 00:37:03 -0300

    Change waiting text

--- src/App.vue
+++ src/App.vue
@@ -31,7 +31,7 @@ export default {
   methods: {
     update() {
       const waiting = Swal.fire({
-        title: 'Aguarde!',
+        title: 'Carregando...',
         type: 'info',
         allowOutsideClick: false,
         showConfirmButton: false
